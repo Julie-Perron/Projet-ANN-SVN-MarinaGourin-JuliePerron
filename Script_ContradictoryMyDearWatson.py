bca3a4c39e3e886fef53cd743bf81f3869afe54d
Julie-Perron <72380162+Julie-Perron@users.noreply.github.com>
2020-10-14 21:07:00 +0200

    Update Script_ContradictoryMyDearWatson.py

--- Script_ContradictoryMyDearWatson.py
+++ Script_ContradictoryMyDearWatson.py
@@ -1,7 +1,14 @@
+#Natural Language Processing (NLP)
+
+ #Bag of words (BOW) 
+ #TF IDF : PCA
+ #Wordzvec
+
+
 import pandas as pd
 import numpy as np
 
 train = pd.read_csv("train.csv",
-                    sep=",",names=["id", "premise", "hypothesis","lang_abv","language","label"])
+                    sep=",",names=["id", "premise", "hypothesis","lang_abv","language","label"])      #Import csv training file
 
 train.head()
