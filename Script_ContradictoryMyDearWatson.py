1be2eb8315864a9059193cbb68c7e1b00762d790
Julie-Perron <72380162+Julie-Perron@users.noreply.github.com>
2020-10-25 18:40:50 +0100

    Update Script_ContradictoryMyDearWatson.py

--- Script_ContradictoryMyDearWatson.py
+++ Script_ContradictoryMyDearWatson.py
@@ -5,10 +5,11 @@
  #Wordzvec
 
 #Library
+#Library
 import pandas as pd
 import numpy as np
 
 train = pd.read_csv("train.csv",
-                    sep=",",names=["id", "premise", "hypothesis","lang_abv","language","label"])      #Import csv training file
+                    sep=",",header=0)      #Import csv training file
 
 train.head()
